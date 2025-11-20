61fccd528a6e49ffda57b2642da54a6951165588
talentestors <talentestors@gmail.com>
2025-11-21 00:05:17 +0800

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,15 @@ import { defineConfig } from "astro/config";
 
 import preact from "@astrojs/preact";
 
+import netlify from "@astrojs/netlify";
+
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://poetic-brioche-ecf1d0.netlify.app/",
   integrations: [preact()],
+  adapter: node({
+    mode: "standalone",
+  }),
 });
