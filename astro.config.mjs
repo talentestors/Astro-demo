5ee861bcae204bb3a7cf0e2349626a4a3c0f0b6c
Yuhiri <talentestors@gmail.com>
2026-01-11 22:54:56 +0800

    Switch adapter from node to netlify with middleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,13 @@ import preact from "@astrojs/preact";
 
 import netlify from "@astrojs/netlify";
 
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://poetic-brioche-ecf1d0.netlify.app/",
   integrations: [preact()],
-  adapter: node({
-    mode: "standalone",
+  adapter: netlify({
+    edgeMiddleware: true,
   }),
 });
